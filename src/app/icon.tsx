8414d5370f82289e2e29fc8d829d267cf690546c
Chanin Law <chaninlawlert@gmail.com>
2023-11-19 18:42:13 +0700

    remove: edge runtime in icon.ts

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,20 +1,14 @@
 import { ImageResponse } from "next/og"
 
-// Route segment config
-export const runtime = "edge"
-
-// Image metadata
 export const size = {
   width: 32,
   height: 32,
 }
 export const contentType = "image/png"
 
-// Image generation
 export default function Icon() {
   return new ImageResponse(
     (
-      // ImageResponse JSX element
       <div
         style={{
           background: "black",
@@ -41,11 +35,6 @@ export default function Icon() {
         </svg>
       </div>
     ),
-    // ImageResponse options
-    {
-      // For convenience, we can re-use the exported icons size metadata
-      // config to also set the ImageResponse's width and height.
-      ...size,
-    }
+    { ...size }
   )
 }
